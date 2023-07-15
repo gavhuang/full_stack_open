2d657ea532698f48613e933f11074201e7d87dbb
gavhuang <47684670+gavhuang@users.noreply.github.com>
2023-07-15 11:05:11 -0400

    Get note example working with Vite

---
+++ part2/notes/src/App.jsx
@@ -0,0 +1,18 @@
+import Note from './components/Note'
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <ul>
+          {notes.map(note =>
+            <Note key={note.id} note={note} />
+          )}
+        </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default App
