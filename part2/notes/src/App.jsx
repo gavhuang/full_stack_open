b47cc67f89d3c8bef958c48e782aa518f5988ac8
gavhuang <47684670+gavhuang@users.noreply.github.com>
2023-07-16 17:52:35 -0400

    Update notes exercise

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,16 +1,49 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = ({ notes: propNotes }) => {
+  const [notes, setNotes] = useState(propNotes)
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes([...notes, noteObject])
+    setNewNote("")
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? "Important" : "All" }
+        </button>
+      </div>
       <ul>
-        <ul>
-          {notes.map(note =>
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">Save</button>
+      </form>
     </div>
   )
 }
