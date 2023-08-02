e12e721c4256f914892c4d62eefe879d44692c90
gavhuang <47684670+gavhuang@users.noreply.github.com>
2023-08-02 13:06:56 -0400

    Formatting

--- part2/phonebook/services/phonebook.js
+++ part2/phonebook/services/phonebook.js
@@ -6,17 +6,13 @@ const getAll = () => axios.get(baseUrl)
 const create = newObject => {
   return axios
     .post(baseUrl, newObject)
-    .catch(
-      alert("Entry could not be created!")
-    )
+    .catch(alert("Entry could not be created!"))
 }
 
 const deleteEntry = id => {
   return axios
     .delete(`${baseUrl}/${id}`)
-    .catch((err) => {
-      alert("Entry could not be deleted!")
-    })
+    .catch(alert("Entry could not be deleted!"))
 }
 
 export default { getAll, create, deleteEntry }
